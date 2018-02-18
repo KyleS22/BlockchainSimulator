9ff55077fc4f98843fd9eacf6085de4a5545154b
Allan Kerr <personal.allankerr@gmail.com>
2018-02-17 23:52:40 -0600

    Added a request server with a jump table for handling different request types

--- node.py
+++ node.py
@@ -1,5 +1,6 @@
 from miner import Miner
 from protos import request_pb2
+from google.protobuf import message
 import time
 import server
 import logging
@@ -23,17 +24,50 @@ class DataServer(server.TCPRequestHandler):
         # TODO forward msg to peers
 
 
+class RequestServer(server.TCPRequestHandler):
+
+    def __init__(self, request, client_address, serv):
+
+        self.request_handlers = {
+            request_pb2.BLOB: self.handle_blob,
+            request_pb2.ALIVE: self.handle_alive
+        }
+
+        server.TCPRequestHandler.__init__(self, request, client_address, serv)
+
+    def receive(self, data):
+
+        req = request_pb2.Request()
+        try:
+            req.ParseFromString(data)
+        except message.DecodeError:
+            logging.error("Error decoding request: %s", data)
+
+        if req.request_type in self.request_handlers:
+            self.request_handlers[req.request_type](data)
+        else:
+            logging.error("Unsupported request type: %s", req.request_type)
+
+    def handle_blob(self, data):
+        pass
+
+    def handle_alive(self, data):
+        pass
+
+
 class Node:
 
     def __init__(self):
 
         self.miner = Miner()
 
-        input_server = server.TCPServer(9999, DataServer)
-        input_server.miner = self.miner
-        server.start_server(input_server)
+        self.request_server = server.TCPServer(10000, RequestServer)
+        self.input_server = server.TCPServer(9999, DataServer)
+        self.input_server.miner = self.miner
 
     def run(self):
+        server.start_server(self.request_server)
+        server.start_server(self.input_server)
         self.miner.mine()
 
 

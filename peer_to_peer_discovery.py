1c30cef22807018cebe4fc4a146a81e1d9965caf
kts135 <25314187+KyleS22@users.noreply.github.com>
2018-02-18 11:41:44 -0600

    Think I got discovery working, having trouble testing locally due to port usage

--- peer_to_peer_discovery.py
+++ peer_to_peer_discovery.py
@@ -5,11 +5,15 @@ Author: Kyle Seidenthal
 """
 import socket
 import threading
+import protos.discovery_pb2 as disc_msg
 
 class UDPDiscover:
     """
     Broadcasts to all nodes on network and waits for response
     """
+
+    IP = 'localhost'
+    PORT = '12345'
 
     def listen(self, port):
         """
@@ -37,8 +41,12 @@ class UDPDiscover:
         """
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, )
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        # TODO: Send a better message
-        sock.sendto("TEST".encode(), ('<broadcast>', port))
+
+        message = disc_msg.DiscoveryMessage()
+        message.message_type = disc_msg.DiscoveryMessage.DISCOVERY
+
+        sock.sendto(message.SerializeToString(), ('<broadcast>', port))
+        print("Sent broadcast")
 
     def UDPServer(self, port):
         """
@@ -50,15 +58,29 @@ class UDPDiscover:
         sock.bind(('', port))   # Empty string -> INADDR_ANY   For some reason <broadcast> does not work on windows.
 
         while True:
-            message, address = sock.recvfrom(1024)
-            print(message)
-            # TODO: Check what type of message it is and handle appropriately
+            in_message, address = sock.recvfrom(1024)
+
+            message = disc_msg.DiscoveryMessage()
+            message.ParseFromString(in_message)
 
             # If discovery message, reply with IP and port for TCP connection
-            sock.sendto("I'm Here!".encode(), address)
+            if message.message_type == disc_msg.DiscoveryMessage.DISCOVERY:
+                reply = disc_msg.DiscoveryMessage()
+                reply.message_type = disc_msg.DiscoveryMessage.CONNECT
+                reply.ip_address = self.IP
+                reply.port = self.PORT
+
+                print("Received discovery")
+                print(address)
+                sock.sendto(message.SerializeToString(), address)
 
             # If IP and port for TCP connection, start TCP server
-            
+            elif message.message_type == disc_msg.DiscoveryMessage.CONNECT:
+                print("Received connect message from: " + message.ip_address + " " + message.port)
+
+            else:
+                print("Something went wrong")
+
 
 
 if __name__ == "__main__":
